97ff6a3cc2df0df30fb6623012385d591147c85f
beeweb <apresyan-koryun@mail.ru>
2021-09-16 15:41:38 +0400

    cleanup

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -3,14 +3,8 @@ import { Global, HttpModule, Module } from '@nestjs/common';
 import { AwsS3Service } from './services/aws-s3.service';
 import { GeneratorService } from './services/generator.service';
 import { ValidatorService } from './services/validator.service';
-import { ConfigService } from './services/config.service';
 
-const providers = [
-  ValidatorService,
-  AwsS3Service,
-  GeneratorService,
-  ConfigService,
-];
+const providers = [ValidatorService, AwsS3Service, GeneratorService];
 
 @Global()
 @Module({
