e23df35cdd84ec300e2372430cf4f980946ff666
beeweb <apresyan-koryun@mail.ru>
2021-09-17 12:21:18 +0400

    add brands

--- src/migrations/1631866355401-insert-brands.ts
+++ src/migrations/1631866355401-insert-brands.ts
@@ -1,12 +1,14 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
-import { ProductTypes } from '../common/constants/product-types';
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { Brands } from '../common/constants/brands';
 
 export class insertBrands1631866355401 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    Brands.map(async (item) => {
+      await queryRunner.query(`INSERT INTO "brands" (name) VALUES ('${item}')`);
+    });
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('DELETE FROM "brands"');
+  }
 }
