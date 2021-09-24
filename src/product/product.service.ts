1961e53134e14530b8e857687cbc5718f2705030
koryun7707 <apresyan-koryun@mail.ru>
2021-09-24 18:47:36 +0400

    execute search color

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -117,7 +117,6 @@ export class ProductService {
               name: `%${search}%`,
               brandName: `%${search}%`,
               size: `%${search}%`,
-              color: `%${search}%`,
               title: `%${search}%`,
             },
           );
