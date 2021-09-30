61e68be904753db8d9d29dc2833d88b22d13bd7c
koryun7707 <apresyan-koryun@mail.ru>
2021-09-30 11:08:16 +0400

    fix env variables

--- src/store_token/storeToken.service.ts
+++ src/store_token/storeToken.service.ts
@@ -5,6 +5,14 @@ import { CreateStoreTokenDto } from './dto/CreateStoreTokenDto';
 import { StoreTokenDto } from './dto/StoreTokenDto';
 import { StoreTokenEntity } from './storeToken.entity';
 import fetch from 'node-fetch';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+const env = process.env.NODE_ENV || 'dev';
+const dotenv_path = path.resolve(process.cwd(), `.${env}.env`);
+const result = dotenv.config({ path: dotenv_path });
+if (result.error) {
+  /* do nothing */
+}
 
 @Injectable()
 export class StoreTokenService {
