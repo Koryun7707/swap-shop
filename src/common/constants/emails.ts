e6e083f8ee7b4c42f1ced424569768bf8b72f7b7
koryun7707 <apresyan-koryun@mail.ru>
2021-10-05 14:27:56 +0400

    add correct emails

---
+++ src/common/constants/emails.ts
@@ -0,0 +1,10 @@
+export const emails = [
+  '@manchester.ac.uk',
+  '@student.manchester.ac.uk',
+  '@postgrad.manchester.ac.uk',
+  '@stu.mmu.ac.uk',
+  '@mmu.ac.uk',
+  '@salford.ac.uk',
+  '@edu.salford.ac.uk',
+  '@swapshop.app',
+];
